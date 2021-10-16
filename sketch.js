0d922414a50a8df120b0c406fbe024cbdf78c9c4
Suhaan2006 <emailsuhaankapoor@gmail.com>
2021-10-16 20:03:12 +0530

    platform1 added

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(250, 350, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -44,6 +44,9 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
+    
+
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +63,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platform1.display();
 }
